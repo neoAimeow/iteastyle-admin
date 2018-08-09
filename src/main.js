d6ee8e2d2419174d698459433a75720489e8a864
映溪 <zhaoyi.w@alibaba-inc.com>
2018-08-09 14:09:04 +0800

    update code

--- src/main.js
+++ src/main.js
@@ -2,8 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueBlu from 'vue-blu'
+import axios from 'axios'
+import VueSilentbox from 'vue-silentbox'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import 'vue-blu/dist/css/vue-blu.min.css'
 
+Vue.use(VueBlu)
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(VueSilentbox)
+Vue.use(require('vue-moment'))
+
+let request = axios.create({
+    baseURL: 'https://iteastyle-api.aimeow.com/',
+    timeout: 1000
+});
+
+Vue.prototype.$ajax = request
 
 new Vue({
   router,
