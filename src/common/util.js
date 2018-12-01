23ec9f517bb91303f9c585f7243f7b42e7db9f84
zhaoyi.w <a18814888787@gmail.com>
2018-12-02 02:43:11 +0800

    update these code

--- src/common/util.js
+++ src/common/util.js
@@ -1,4 +1,5 @@
 import axios from "axios/index";
+import {Notification, MessageBox} from 'element-ui';
 
 export let request = axios.create({
     baseURL: 'https://iteastyle-api-test.aimeow.com/',
@@ -10,4 +11,32 @@ export function getQiniuToken(TokenCallback) {
         .then(function (response) {
             TokenCallback(response.data.model)
         })
+}
+
+
+export function getQiniuToken(TokenCallback) {
+    request.get('/upload/getUploadToken', {})
+        .then(function (response) {
+            TokenCallback(response.data.model)
+        })
+}
+
+export function showNotify(content) {
+    Notification({
+        title: '提示',
+        message: content
+    })
+}
+
+export function showAlert(content) {
+    return new Promise(function (resolve, reject) {
+        MessageBox.confirm(content, '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消'
+        }).then(() => {
+            resolve();
+        }).catch(() => {
+            reject();
+        })
+    })
 }
